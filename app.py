9928fbda61883dddeaff586d94aab53c55b1c975
orotmate <orotmate@gmail.com>
2020-11-06 13:55:03 +0100

    https://www.youtube.com/watch?v=EeT3Ore4Sao&t=1s

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect
 import config, csv
 from binance.client import Client
 from binance.enums import *
@@ -6,6 +6,7 @@ from binance.enums import *
 # from jinja2 import Template
 
 app = Flask(__name__)
+app.secret_key = 'rarnakjngvaédfughpeiorufgaékldjsfvn4523j5n2'
 
 client = Client(config.API_KEY, config.API_SECRET, tld='com')
 
@@ -13,20 +14,27 @@ client = Client(config.API_KEY, config.API_SECRET, tld='com')
 def index():
     title = 'CoinView'
 
-    info = client.get_account()
+    account = client.get_account()
 
-    balances = info['balances']
+    balances = account['balances']
 
-    return render_template('index.html', title=title, my_balances = balances)
+    exchange_info = client.get_exchange_info()
+    symbols = exchange_info['symbols']
 
-@app.route('/buy')
+    return render_template('index.html', title=title, my_balances = balances, symbols = symbols)
+
+@app.route('/buy', methods = ['POST'])
 def buy():
-    order = client.create_order(symbol='LTC',side=SIDE_BUY,
-    type=ORDER_TYPE_LIMIT,
-    timeInForce=TIME_IN_FORCE_GTC,
-    quantity=,
+    print(request.form)
+    try:
+        order=client.create_order(request.form['symbol'],
+            side=SIDE_BUY,
+            type=ORDER_TYPE_MARKET,
+            quantity=request.form['quantity'])
+    except Exception as e:
+        flash(e.massage, "error")
 
-    return 'buy'
+    return redirect('/')
 
 @app.route('/sell')
 def sell():
